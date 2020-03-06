0e3a02488b08156cf7b8cb078fbefba7dbee3584
Jerome Mico Jaropojop <jmjaropojop92@gmail.com>
2020-03-06 14:01:53 +0800

    *Initial Commit*; Installed dependencies and created scripts for concurrently; Implemented connection to mongoDB Database; Added folders to ignore in '.gitignore' file; created folder structure for the backend;

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDb Database Connected');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
