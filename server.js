fa129b9a3cc361b12dc6ebd3b049e8733d3e316a
jmjaro-dev <jmjaropojop92@gmail.com>
2020-05-06 18:30:51 +0800

    Deployment Setup Done.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -14,6 +15,16 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/tickets', require('./routes/tickets'));
 app.use('/api/comments', require('./routes/comments'));
+
+// Serve static assets if in Production
+if(process.env.NODE_ENV === 'production') {
+  // Set a static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
